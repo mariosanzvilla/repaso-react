eb9625d4fd504654e8bca2c9c360c6e2d8525e72
Mario Sanz <mariosanzvilla@gmail.com>
2022-10-29 10:10:10 +0200

    cambio homepage 5

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Repaso modificado para subir a pro 4
+          Repaso modificado para subir a pro 5
         </p>
         <PrimerComponente/>
         <hr/>

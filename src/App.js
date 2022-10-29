e13ade530d95046e5f92adbbf04ba13ff26b3c8d
Mario Sanz <mariosanzvilla@gmail.com>
2022-10-29 09:16:31 +0200

    cambiada home

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Repaso
+          Repaso modificado para subir a pro
         </p>
         <PrimerComponente/>
         <hr/>
